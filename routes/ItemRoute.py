664eb17d3ddafafcc1473d52c704afc5254b3922
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-27 20:40:03 +0530

    Update

--- routes/ItemRoute.py
+++ routes/ItemRoute.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from controllers.ItemController import add_Item
+from controllers.ItemController import add_Item, find_all_Items
 from models.Items import Item
 
 router = APIRouter()
@@ -7,3 +7,8 @@ router = APIRouter()
 @router.post('/add_item')
 async def create_item(item: Item):
     return await add_Item(item)
+
+
+@router.get('/all_imtes')
+async def all_items():
+    return await find_all_Items()
